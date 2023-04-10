ea5fd75815b22cfd40688791555b76e0511c22c8
Meelis Reha <meelisreha@hotmail.com>
2023-04-10 21:00:07 +1000

    Uuendatud properties ja lisatud OpenApiConfigratuin Swaggeri jaoks

---
+++ src/main/java/com/example/workspacermk/Swagger/OpenApiConfiguration.java
@@ -0,0 +1,17 @@
+package com.example.workspacermk.Swagger;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfiguration {
+    @Bean
+    public OpenAPI usersMicroserviceOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Api RMK kodutööle")
+                        .description("RMK kodutöö")
+                        .version("1.0"));
+    }
+}
